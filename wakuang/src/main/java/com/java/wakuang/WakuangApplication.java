f56cbacfa3a527ccb2fec860cec1294288b111b6
Zhongzhe <Zhongzhe@users.noreply.github.com>
2018-07-05 16:38:45 +0800

    Add files via upload

---
+++ wakuang/src/main/java/com/java/wakuang/WakuangApplication.java
@@ -0,0 +1,12 @@
+package com.java.wakuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakuangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakuangApplication.class, args);
+	}
+}
